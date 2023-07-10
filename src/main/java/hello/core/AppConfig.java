ed9da637c111c29f8f92b3fd705c679dd9823e83
ChoiWonYu <phillip07290@naver.com>
2023-07-10 19:03:53 +0900

    fearefactor: apply spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -5,20 +5,28 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberServiceImpl memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderServiceImpl orderService(){
        return  new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
